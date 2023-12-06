fab2ed8fb32524645255babdd401d5c522e7bee0
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-06 23:29:14 +0530

    fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,9 +4,10 @@ const main =  async() =>{
   const Nebulaxcontract = await hre.ethers.getContractFactory("Nebulax")
 
   const deployeraddress = '0x514910771AF9Ca656af840dff83E8264EcF986CA';
-  
+  const recieveraddress = '0xa151660a77f223e87298de16ee5bb7447982b62f';
 
-  const deployContract = await Nebulaxcontract.deploy('','0xa151660a77f223e87298de16ee5bb7447982b62f'); 
+
+  const deployContract = await Nebulaxcontract.deploy(); 
   await deployContract.waitForDeployment();
   
   console.log(`Contract deployed successfully to ${deployContract.target}`);
