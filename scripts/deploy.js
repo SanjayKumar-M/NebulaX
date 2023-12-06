946126889916dd9969d6925a7018f8553aed3a8f
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-12-06 23:28:41 +0530

    deploy update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,10 @@ const hre = require("hardhat")
 const main =  async() =>{
   const Nebulaxcontract = await hre.ethers.getContractFactory("Nebulax")
 
-  const deployContract = await Nebulaxcontract.deploy('0x514910771AF9Ca656af840dff83E8264EcF986CA','0xa151660a77f223e87298de16ee5bb7447982b62f'); 
+  const deployeraddress = '0x514910771AF9Ca656af840dff83E8264EcF986CA';
+  
+
+  const deployContract = await Nebulaxcontract.deploy('','0xa151660a77f223e87298de16ee5bb7447982b62f'); 
   await deployContract.waitForDeployment();
   
   console.log(`Contract deployed successfully to ${deployContract.target}`);
